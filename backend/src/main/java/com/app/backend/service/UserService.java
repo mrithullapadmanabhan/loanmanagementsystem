56fd447b9e212744b1dced26aa5a97a52033d578
kmishul <coolmishul@gmail.com>
2023-08-11 12:09:14 +0530

    Backend Login Functionality

--- backend/src/main/java/com/app/backend/service/UserService.java
+++ backend/src/main/java/com/app/backend/service/UserService.java
@@ -1,9 +1,12 @@
 package com.app.backend.service;
 
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.app.backend.dao.UserRepository;
+import com.app.backend.model.LoginModel;
 import com.app.backend.model.User;
 
 @Service
@@ -16,5 +19,21 @@ public class UserService {
 		User obj=userRepository.save(user);
 		return obj;
 		
+	}
+
+	public String validateUser(LoginModel lm) {
+
+		String result="";
+		User user=null;
+		Optional<User>obj=userRepository.findById(lm.getEmail());
+		if(obj.isPresent())
+			user=obj.get();
+		if(user==null)
+			result="Invalid user";
+		else if(lm.getPassword().equals(user.getPassword()))
+			result="Login success";
+		else result="Login failed";
+		//System.out.println(result);
+		return result;
 	}
 }
