dbe65b1ec4131ba5c8c5370a5eac55c6d6e6c5ed
Jesima Abudhahir <ajesima02@gmail.com>
2023-08-18 13:27:45 +0530

    minor changes

--- backend/src/test/java/com/app/backend/BackendApplicationTests.java
+++ backend/src/test/java/com/app/backend/BackendApplicationTests.java
@@ -2,8 +2,9 @@ package com.app.backend;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
+import org.springframework.context.annotation.ComponentScan;
+@ComponentScan(basePackages = "com")
+@SpringBootTest(classes = com.app.backend.CategoryControllerTest.class)
 class BackendApplicationTests {
 
 	@Test
