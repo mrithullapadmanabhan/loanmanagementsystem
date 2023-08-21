9dfc9a2fd8b7fcd842fd305d81fd165a760daeb8
Yash Goel <goelyashgoel30@gmail.com>
2023-08-21 18:07:14 +0530

    logout button added

--- frontend/src/routes/Root/index.tsx
+++ frontend/src/routes/Root/index.tsx
@@ -4,7 +4,8 @@ import {
   faHome,
   faMoneyBillWave,
   faChair,
-  faUser
+  faUser,
+  faRightFromBracket
 } from "@fortawesome/free-solid-svg-icons";
 
 import Sidebar from 'components/Sidebar';
@@ -16,12 +17,14 @@ const employeeOptions = [
   { label: "View Loans", Icon: faMoneyBillWave, route: "/loans" },
   { label: "View Items Purchased", Icon: faChair, route: "/employee/items" },
   { label: "Apply for loan", Icon: faMoneyBillWave, route: "/loan/apply" },
+  { label: "Logout", Icon: faRightFromBracket, route: "login" }
 ];
 
 const adminOptions = [
   { label: "View Loan cards", Icon: faMoneyBillWave, route: "/admin/loan-card/all" },
   { label: "View Employees", Icon: faUser, route: "admin/employee/all" },
   { label: "View Items", Icon: faChair, route: "admin/item/all" },
+  { label: "Logout", Icon: faRightFromBracket, route: "login" }
 ];
 
 const Root = () => {
@@ -39,7 +42,7 @@ const Root = () => {
   return (
     <>
       <div className={ loggedIn ? 'overflow-hidden ml-0 sm:ml-[240px]' : 'overflow-hidden ml-0' }>
-        { loggedIn && <Sidebar options = {admin?adminOptions:employeeOptions} /> }
+        { loggedIn && <Sidebar options = {admin?adminOptions:employeeOptions} isAdmin={admin} /> }
         <Outlet />
       </div>
     </>
