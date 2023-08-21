9f2dc5e9d70bdf29c230fa804fd86c17cd1f0df0
mrithullapadmanabhan <mrithusree1@gmail.com>
2023-08-21 06:56:40 +0530

    Test for EmpplyeeController,EmployeeLoanController

--- backend/src/test/java/com/app/backend/EmployeeLoanControllerTest.java
+++ backend/src/test/java/com/app/backend/EmployeeLoanControllerTest.java
@@ -6,6 +6,7 @@ import java.util.List;
 import java.util.UUID;
 
 import org.springframework.http.MediaType;
+import org.hamcrest.Matchers;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.ArgumentMatchers;
@@ -28,7 +29,9 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 import com.app.backend.communication.request.EmployeeRegisterRequest;
+import com.app.backend.communication.request.LoanCreationRequest;
 import com.app.backend.communication.response.EmployeeRegisterResponse;
+import com.app.backend.communication.response.LoanCreationResponse;
 import com.app.backend.model.Employee;
 import com.app.backend.model.EmployeeLoan;
 import com.app.backend.repository.CategoryRepository;
@@ -53,7 +56,9 @@ import com.fasterxml.jackson.databind.SerializationFeature;
 @RunWith(SpringRunner.class)
 @WebMvcTest
 @AutoConfigureMockMvc(addFilters=false)
-public class EmployeeControllerTest {
+public class EmployeeLoanControllerTest {
+
+
     @Autowired
     private MockMvc mvc;
 
@@ -118,66 +123,58 @@ public class EmployeeControllerTest {
      * @return
      * @throws Exception
      */
-    @Test
-    public void register() throws Exception{
-        EmployeeRegisterRequest employee = new EmployeeRegisterRequest();
-        EmployeeRegisterResponse emplresponse = new EmployeeRegisterResponse();
-        UUID num = UUID.fromString("acde070d-8c4c-4f0d-9d8a-162843c10333");
-        Date date = new Date(0);
+
+     @Test
+     public void create() throws Exception{
         
-        emplresponse.setEmployeeID(num);
-        employee.setEmail("myEmail@gmail.com");
-        employee.setGender("myGender");
-        employee.setDepartment("myDepartment");
-        employee.setDesignation("myDesignation");
-        employee.setName("myName");
-        employee.setDepartment("myPassword@123");
-        employee.setDob(date);
-        employee.setDoj(date);
+        LoanCreationRequest loanRequest = new LoanCreationRequest();
+        LoanCreationResponse loanResponse = new LoanCreationResponse();
+        UUID num = UUID.fromString("acde070d-8c4c-4f0d-9d8a-162843c10333");
+        UUID num1 = UUID.fromString("123e4567-e89b-12d3-a456-426614174000");
+        loanRequest.setEmployeeID(num);
+        loanRequest.setMakeID(num1);
 
-        // employee.setDob(Date.now());
-        // employee.setName("Myname");
-        // employee.setDepartment("MyDpt");
-        // employee.setDesignation("MyDesignation");
+        loanResponse.setLoanID(num);
+        Mockito.when(employeeLoanService.create(ArgumentMatchers.any())).thenReturn(loanResponse);
+        String request = mapper.writeValueAsString(loanRequest);
+        String response = mapper.writeValueAsString(loanResponse);
 
-        Mockito.when(employeeService.register(ArgumentMatchers.any())).thenReturn(emplresponse);
-        String json = mapper.writeValueAsString(employee);
-        String resp = mapper.writeValueAsString(emplresponse);
-        MvcResult requestResult = mvc.perform(post("/api/employee/register")
+        MvcResult requestResult = mvc.perform(post("/api/loan/create")
         .contentType(MediaType.APPLICATION_JSON)
         .characterEncoding("utf-8")
-        .content(json))
+        .content(request))
         .andExpect(status().isCreated())
         .andReturn();
 
         String result = requestResult.getResponse().getContentAsString();
         System.out.print(result);
-        assertEquals(resp, result);
+        assertEquals(response, result);
 
-    }
 
-    @Test
-    public void get() throws Exception {
-        Employee employee = new Employee();
-        UUID num = UUID.fromString("acde070d-8c4c-4f0d-9d8a-162843c10333");
+     }
+
+     @Test
+     public void markCompleted() throws Exception{
+        EmployeeLoan employeeLoan = new EmployeeLoan();
+
         Date date = new Date(0);
-
-        employee.setId(num);
-        // employee.setLoans(null);
-        employee.setDepartment(null);
-        employee.setName("name");
-        employee.setDesignation("designation");
-        employee.setDob(date);
-        employee.setDoj(date);
-        employee.setGender("female");
-
-        List<Employee> list = new ArrayList<>();
-        list.add(employee);
-        Mockito.when(employeeService.get()).thenReturn(list);
+        UUID num = UUID.fromString("acde070d-8c4c-4f0d-9d8a-162843c10333");
+        String id = "acde070d-8c4c-4f0d-9d8a-162843c10333";
+        employeeLoan.setId(num);
+        employeeLoan.setIssueDate(date);
+        // employeeLoan.setItem(null);
+        // employeeLoan.setLoan(null);
+        employeeLoan.setStatus(null);
+        // employeeLoan.setEmployee(null);
+        List<EmployeeLoan> list = new ArrayList<>();
+        list.add(employeeLoan);
+        Mockito.when(employeeLoanService.setCompleted(num)).thenReturn(id);
       //   String string = "Updated Successfully";
-        mvc.perform(get("/api/employee")
-        .contentType(MediaType.APPLICATION_JSON))
-            .andExpect(status().isAccepted());
-            // .andExpect(content().string(id));
-    }
+        mvc.perform(get("/api/loan/acde070d-8c4c-4f0d-9d8a-162843c10333/status/completed"))
+      //   .contentType(MediaType.APPLICATION_JSON)
+            .andExpect(status().isAccepted())
+            .andExpect(content().string(id));
+
+     }
+
 }
