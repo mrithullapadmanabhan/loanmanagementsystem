abc72555229b27f179ee4ad8292b3ef2d522c05d
Yash Goel <goelyashgoel30@gmail.com>
2023-08-23 14:35:11 +0530

    Refactored some code

--- frontend/src/routes/ViewLoans/index.tsx
+++ frontend/src/routes/ViewLoans/index.tsx
@@ -1,3 +1,4 @@
+import Table from 'components/Table'
 import { useEffect, useState } from 'react'
 import { useNavigate } from 'react-router-dom'
 import { getEmployeeLoansApi } from 'service/loan'
@@ -18,40 +19,37 @@ function ViewLoans() {
 
   }
 
+
+  
+  const fields: any = [
+    {
+      key: "id",
+      label: "Loan Id"
+    },
+    {
+      key: "status",
+      label: "Issue Status"
+    },
+    {
+      key: "item?.description",
+      label: "Loan Category"
+    },
+    {
+      key: "item?.make?.name",
+      label: "Loan Duration"
+    },
+    {
+      key: "issueDate",
+      label: "Issue date"
+    }
+  ]
+
   return (
     <div>
       <div className="p-4 sm:p-8 md:p-11 flex flex-col gap-12">
         <div className="w-full  flex flex-col justify-center gap-4 mb-6 lg:mb-0">
           <h1 className=" text-xl font-semibold mb-5">Loan cards availed</h1>
-          <div className="w-full overflow-auto xl:overflow-visible">
-            <table className="min-w-full">
-              <thead className="text-base font-medium ">
-                <tr>
-                  <td className="min-w-[80px] py-4 text-left">Loan Id</td>
-                  <td className="min-w-[100px] py-4 px-2 text-left">
-                    Loan Category
-                  </td>
-                  <td className="min-w-[110px] py-4 px-2 text-left">
-                    Duration
-                  </td>
-                  <td className="min-w-[70px] py-4 px-2 text-left">
-                    Issue Date
-                  </td>
-                </tr>
-              </thead>
-
-              <tbody className="text-sm text-todayQ-black">
-                {tableData.map((result, index) => (
-                  <tr key={index}>
-                    <td className="text-left">{result?.id}</td>
-                    <td className="text-black py-4 px-2 text-left">{result?.loan?.category?.name}</td>
-                    <td className="py-4 px-2 text-left">{result?.loan?.duration}</td>
-                    <td className="py-4 px-2 text-left">{result?.issueDate}</td>
-                  </tr>
-                ))}
-              </tbody>
-            </table>
-          </div>
+          <Table fields={fields} data={tableData} />
         </div>
       </div>
     </div>
