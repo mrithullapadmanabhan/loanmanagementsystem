1edb37dea2bf858b49e0338d7ddd33066d61a67b
Yash Goel <goelyashgoel30@gmail.com>
2023-08-23 15:04:49 +0530

    UI fixes

--- frontend/src/components/Table/index.tsx
+++ frontend/src/components/Table/index.tsx
@@ -17,18 +17,18 @@ const Table = ({ fields, data, actions }: FormType) => {
           <p>No records available.</p>
         ) : (
 
-          <table className="min-w-full">
+          <table className="min-w-full border-collapse border border-slate-400">
             <thead className="text-base font-medium ">
               <tr>
-                {fields.map((field) => <td className="min-w-[80px] py-4 text-left">{field.label}</td>)}
+                {fields.map((field) => <td className="border border-slate-300 text-lg font-medium min-w-[80px] py-4 text-center">{field.label}</td>)}
               </tr>
             </thead>
 
-            <tbody className="text-sm text-todayQ-black">
+            <tbody className="text-sm">
               {data.map((result, index) => (
                 <tr key={index}>
                   {fields.map((field) => (
-                    <td className="py-4 px-2 text-left">{field.key != "actions" ? result?.[field.key] : <div className="flex gap-4">{actions?.map((action) => <div onClick={() => action.onClick(index)}>{action.label}</div>)}</div>}</td>
+                    <td className="border border-slate-300 py-4 px-2 text-center">{field.key != "actions" ? result?.[field.key] : <div className="flex gap-4 justify-center	">{actions?.map((action) => <div onClick={() => action.onClick(index)}>{action.label}</div>)}</div>}</td>
                   ))}
                 </tr>
 
