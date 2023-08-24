6f1bb23bfa5e9bf5e863c7e8662065458028bfdb
Jesima Abudhahir <ajesima02@gmail.com>
2023-08-24 23:20:31 +0530

    added auth controller tests

--- backend/src/test/java/com/app/backend/AuthenticationControllerTest.java
+++ backend/src/test/java/com/app/backend/AuthenticationControllerTest.java
@@ -1,73 +1,135 @@
 package com.app.backend;
 
-import static org.mockito.Mockito.when;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-import static org.springframework.test.web.servlet.setup.MockMvcBuilders.standaloneSetup;
 
-import org.junit.jupiter.api.BeforeEach;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
 import org.junit.jupiter.api.Test;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.mockito.MockitoAnnotations;
+import org.junit.runner.RunWith;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
 
 import com.app.backend.communication.request.JWTLoginRequest;
 import com.app.backend.communication.request.JWTRefreshRequest;
 import com.app.backend.communication.response.JWTResponse;
-import com.app.backend.controller.AuthenticationController;
+import com.app.backend.model.Role;
+import com.app.backend.model.RoleEnum;
+import com.app.backend.service.CategoryService;
+import com.app.backend.service.EmployeeLoanService;
+import com.app.backend.service.EmployeeService;
+import com.app.backend.service.ItemCardService;
+import com.app.backend.service.LoanCardService;
+import com.app.backend.service.MakeService;
 import com.app.backend.service.auth.AuthenticationService;
+import com.app.backend.service.auth.JWTService;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
 
+@RunWith(SpringRunner.class)
+@WebMvcTest
+@AutoConfigureMockMvc(addFilters = false)
 public class AuthenticationControllerTest {
 
-    @Mock
-    private AuthenticationService authService;
+    @Autowired
+    private MockMvc mvc;
 
-    @InjectMocks
-    private AuthenticationController authController;
+    @MockBean
+    private CategoryService categoryService;
 
-    private MockMvc mockMvc;
+    @MockBean
+    private EmployeeLoanService employeeLoanService;
 
-    @BeforeEach
-    public void setup() {
-        MockitoAnnotations.initMocks(this);
-        mockMvc = standaloneSetup(authController).build();
+    @MockBean
+    private EmployeeService employeeService;
+
+    @MockBean
+    private ItemCardService itemCardService;
+
+    @MockBean
+    private LoanCardService loanCardService;
+
+    @MockBean
+    private MakeService makeService;
+
+    @MockBean
+    private AuthenticationService authenticationService;
+
+    @MockBean
+    private JWTService jwtService;
+
+    ObjectMapper mapper = new ObjectMapper()
+            .findAndRegisterModules()
+            .disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
+
+    @Test
+    public void loginTest() throws Exception {
+
+        JWTLoginRequest jwtLoginRequest = new JWTLoginRequest("username@gmail.com", "Password123*");
+        String accessToken = "eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJhc2h1dG9zaHNocm01MjlAZ21haWwuY29tIiwiaWF0IjoxNjkyNjk2ODU0LCJleHAiOjE2OTI2OTc0NTR9.VpWwD2phNbMVMIB87AplHAP59g2wX0N5qgL0RUZEg3w";
+        String refreshToken = "eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJhc2h1dG9zaHNocm01MjlAZ21haWwuY29tIiwiaWF0IjoxNjkyNjk2ODU0LCJleHAiOjE2OTI3MDA0NTR9.gJzGfE8cq98dR9Su466mFMQ-0syeQxcsjs3ILzakVAg";
+        Role role = new Role();
+        role.setId(UUID.randomUUID());
+        role.setName(RoleEnum.USER);
+        List<Role> roles = new ArrayList<Role>();
+        roles.add(role);
+        UUID id = UUID.randomUUID();
+        JWTResponse jwtResponse = new JWTResponse(accessToken, refreshToken, roles, id);
+
+        Mockito.when(authenticationService.login(jwtLoginRequest)).thenReturn(jwtResponse);
+
+        String loginRequestString = mapper.writeValueAsString(jwtLoginRequest);
+        String jwtResponseString = mapper.writeValueAsString(jwtResponse);
+
+        MvcResult requestResult = mvc.perform(post("/api/auth/login")
+                .contentType(MediaType.APPLICATION_JSON)
+                .characterEncoding("utf-8")
+                .content(loginRequestString))
+                .andExpect(status().isOk())
+                .andReturn();
+
+        String loginRequestResult = requestResult.getResponse().getContentAsString();
+        assertEquals(jwtResponseString, loginRequestResult);
     }
 
     @Test
-    public void testLogin() throws Exception {
-        JWTLoginRequest loginRequest = new JWTLoginRequest("username", "password");
-        JWTResponse jwtResponse = new JWTResponse();
+    public void refreshTest() throws Exception {
 
-        when(authService.login(loginRequest)).thenReturn(jwtResponse);
+        JWTRefreshRequest jwtRefreshRequest = new JWTRefreshRequest(
+                "eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJhc2h1dG9zaHNocm01MjlAZ21haWwuY29tIiwiaWF0IjoxNjkyNjk2ODU0LCJleHAiOjE2OTI3MDA0NTR9.gJzGfE8cq98dR9Su466mFMQ-0syeQxcsjs3ILzakVAg");
+        String accessToken = "eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJhc2h1dG9zaHNocm01MjlAZ21haWwuY29tIiwiaWF0IjoxNjkyNjk2ODU0LCJleHAiOjE2OTI2OTc0NTR9.VpWwD2phNbMVMIB87AplHAP59g2wX0N5qgL0RUZEg3w";
+        String refreshToken = "eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJhc2h1dG9zaHNocm01MjlAZ21haWwuY29tIiwiaWF0IjoxNjkyNjk2ODU0LCJleHAiOjE2OTI3MDA0NTR9.gJzGfE8cq98dR9Su466mFMQ-0syeQxcsjs3ILzakVAg";
+        Role role = new Role();
+        role.setId(UUID.randomUUID());
+        role.setName(RoleEnum.USER);
+        List<Role> roles = new ArrayList<Role>();
+        roles.add(role);
+        UUID id = UUID.randomUUID();
+        JWTResponse jwtResponse = new JWTResponse(accessToken, refreshToken, roles, id);
 
-        mockMvc.perform(post("/login")
+        Mockito.when(authenticationService.refresh(jwtRefreshRequest)).thenReturn(jwtResponse);
+
+        String refreshRequestString = mapper.writeValueAsString(jwtRefreshRequest);
+        String jwtResponseString = mapper.writeValueAsString(jwtResponse);
+
+        MvcResult requestResult = mvc.perform(post("/api/auth/refresh")
                 .contentType(MediaType.APPLICATION_JSON)
-                .content(asJsonString(loginRequest)))
-                .andExpect(status().isOk());
-    }
+                .characterEncoding("utf-8")
+                .content(refreshRequestString))
+                .andExpect(status().isOk())
+                .andReturn();
 
-    @Test
-    public void testRefresh() throws Exception {
-        JWTRefreshRequest refreshRequest = new JWTRefreshRequest("refreshToken");
-        JWTResponse jwtResponse = new JWTResponse();
-
-        when(authService.refresh(refreshRequest)).thenReturn(jwtResponse);
-
-        mockMvc.perform(post("/refresh")
-                .contentType(MediaType.APPLICATION_JSON)
-                .content(asJsonString(refreshRequest)))
-                .andExpect(status().isOk());
-    }
-
-    // Helper method to convert objects to JSON string
-    private static String asJsonString(final Object obj) {
-        try {
-            return new ObjectMapper().writeValueAsString(obj);
-        } catch (Exception e) {
-            throw new RuntimeException(e);
-        }
+        String refreshRequestResult = requestResult.getResponse().getContentAsString();
+        assertEquals(jwtResponseString, refreshRequestResult);
     }
 }
