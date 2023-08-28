a78b96d3a171df7fe59733675170b10d383a108d
Ashutosh Sharma <ashutoshshrm529@gmail.com>
2023-08-28 11:46:06 +0530

    UI fixes for sidebar

--- frontend/src/routes/NotFound/index.tsx
+++ frontend/src/routes/NotFound/index.tsx
@@ -1,33 +1,31 @@
-import { Link, useRouteError } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 
 export default function NotFound() {
-  const error:any = useRouteError();
-  console.error(error);
+  const navigate = useNavigate();
 
   return (
     <>
       <div className="flex items-center justify-center h-screen">
         <div className="bg-white">
           <div className="flex flex-col items-center">
-            <h1 className="font-bold text-3xl text-blue-600 lg:text-6xl">
+            <h1 className="font-bold text-3xl text-blue-900 lg:text-6xl mb-6">
               404
             </h1>
 
             <h6 className="mb-2 text-2xl font-bold text-center text-gray-800 md:text-3xl">
-              <span className="text-red-500">Oops!</span> Page{" "}
-              {error.statusText}
+              Oops! Page not found!
             </h6>
 
             <p className="mb-4 text-center text-gray-500 md:text-lg">
               The page you’re looking for doesn’t exist.
             </p>
 
-            <Link
-              to="/"
-              className="px-5 py-2 rounded-md text-blue-100 bg-blue-600 hover:bg-blue-700"
+            <div
+              onClick={() => navigate(-1)}
+              className="px-5 py-2 rounded-md text-gray-100 bg-blue-900 hover:bg-blue-700"
             >
-              Go home
-            </Link>
+              Go back
+            </div>
           </div>
         </div>
       </div>
